783f25c02c26932cee07ebf4765ca618a4933fb9
CHOKONEX <47723659+CHOKONEX@users.noreply.github.com>
2020-04-19 15:35:16 +0200

    DAPPER 8

--- App.Net.Core.Features.Test/Modules/TestEnums/Impl/EnumModule.cs
+++ App.Net.Core.Features.Test/Modules/TestEnums/Impl/EnumModule.cs
@@ -2,5 +2,22 @@
 {
     public class EnumModule
     {
+        public enum EUnitOfMeasurement : int
+        {
+            [Description("UN")]
+            Unity = 1,
+
+            [Description("MG")]
+            Milligram = 2,
+
+            [Description("G")]
+            Gram = 3,
+
+            [Description("KG")]
+            Kilogram = 4,
+
+            [Description("L")]
+            Liter = 5
+        }
     }
 }
