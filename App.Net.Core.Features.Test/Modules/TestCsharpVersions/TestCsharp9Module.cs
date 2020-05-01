9d433f94c68bc7e7265b3883c38b4ebb59b7a3ae
CHOKONEX <47723659+CHOKONEX@users.noreply.github.com>
2020-05-01 13:25:29 +0200

    CSHARP 9

---
+++ App.Net.Core.Features.Test/Modules/TestCsharpVersions/TestCsharp9Module.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace App.Net.Core.Features.Test.Modules.TestCsharpVersions.TestCSharp6
+{
+    /*C# 9*/
+    /*********/
+    /*
+     * 
+    */
+    public static class TestCsharp9Module
+    {
+        /**/
+        /*********/
+        
+        public partial class Student
+        {
+            public void Test()
+            {
+
+            }
+        }
+    }
+}
